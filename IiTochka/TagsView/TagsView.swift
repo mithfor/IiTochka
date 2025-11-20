be397a3448f1203770113d955606e66ade815782
Dmitriy Voronin <devoronin@gmail.com>
2025-11-20 20:47:32 +0300

    Refactored TagsView with adding Tag Tap Handling

--- IiTochka/TagsView/TagsView.swift
+++ IiTochka/TagsView/TagsView.swift
@@ -14,26 +14,66 @@ struct TagViewItem: Hashable, Identifiable {
     var isSelected: Bool
 }
 
+struct TagView: View {
+    
+    let tag: TagViewItem
+    let onTap: (() -> Void)?
+    
+    init(tag: TagViewItem, onTap: (() -> Void)? = nil) {
+        self.tag = tag
+        self.onTap = onTap
+    }
+    
+    var body: some View {
+        Text(tag.title)
+            .padding(EdgeInsets(top: 6, leading: 12, bottom: 6, trailing: 12))
+            .background(backgroundView)
+            .clipShape(RoundedRectangle(cornerRadius: 14,
+                                        style: .continuous
+                                       )
+            )
+            .font(.system(size: 13, weight: .regular))
+            .foregroundStyle(Color.white)
+            .onTapGesture {
+                onTap?()
+            }
+    }
+    
+    private var backgroundView: some View {
+        Group {
+            if tag.isSelected {
+                selectedBackgroundColor
+            } else {
+                backgroundColor
+            }
+        }
+    }
+    
+    private var selectedBackgroundColor: some View {
+        Color.green
+            .opacity(0.3)
+            .blur(radius: 0.15)
+    }
+    
+    private var backgroundColor: some View {
+        Color.black
+            .opacity(0.3)
+            .blur(radius: 0.15)
+    }
+}
 
 struct TagsView: View {
     
     @Binding var tags: [TagViewItem]
+    var onTagTap: ((TagViewItem) -> Void)?
     
     var body: some View {
         ScrollView(.horizontal, showsIndicators: false) {
             HStack(alignment: .center) {
                 ForEach(tags, id: \.id) { tag in
-                    Text(tag.title)
-                        .padding(EdgeInsets(top: 6, leading: 12, bottom: 6, trailing: 12))
-                        .background(
-                            .black
-                            .opacity(0.3)
-                        )
-                        .blur(radius: 0.15)
-                        .clipShape(RoundedRectangle(cornerRadius: 14,
-                                                    style: .continuous))
-                        .font(.system(size: 13, weight: .regular))
-                        .foregroundStyle(Color.white)
+                    TagView(tag: tag) {
+                        onTagTap?(tag)
+                    }
                 }
             }
         }
@@ -49,10 +89,25 @@ struct TagsViewContainer: View {
     ]
     
     var body: some View {
+        
+        // Multitag sellection
+//        TagsView(tags: $tags) { tag in
+//            handleTagTap(tag)
+//        }
+        
+        // No tag selection
         TagsView(tags: $tags)
+    }
+    
+    private func handleTagTap(_ tag: TagViewItem) {
+        if let index = tags.firstIndex(where: { $0.id == tag.id}) {
+            tags[index].isSelected.toggle()
+        }
     }
 }
 
 #Preview {
     TagsViewContainer()
+        .padding()
+        .background(Color.gray.opacity(0.1))
 }
